b9a077c066bfa54d97a30a6ec3c53b0463e7dc3d
xxxREDBORNxxx <102207872+xxxREDBORNxxx@users.noreply.github.com>
2022-10-16 19:40:38 +0400

    add import MainPage and change test_guest_can_go_to_login_page

--- test_main_page.py
+++ test_main_page.py
@@ -1,8 +1,13 @@
-from selenium.webdriver.common.by import By
+import time
+from .pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
-    browser.get(link)
-    login_link = browser.find_element(By.CSS_SELECTOR, "#login_link")
-    login_link.click()
+    page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
+    page.open()                      # открываем страницу
+    time.sleep(5)
+    page.go_to_login_page()          # выполняем метод страницы — переходим на страницу логин
+    time.sleep(5)
+    page.should_be_login_link()
+    time.sleep(5)
