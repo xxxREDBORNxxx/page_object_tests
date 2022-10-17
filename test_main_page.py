d6b3f9d8b87c426c36cdc32ec56ffe390b4ab8c1
xxxREDBORNxxx <102207872+xxxREDBORNxxx@users.noreply.github.com>
2022-10-17 12:11:05 +0400

    update test_main_page (delete time.sleep)

--- test_main_page.py
+++ test_main_page.py
@@ -1,4 +1,3 @@
-import time
 from .pages.main_page import MainPage
 from .pages.login_page import LoginPage
 
@@ -7,11 +6,8 @@ def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link)  # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()  # открываем страницу
-    time.sleep(5)
     page.go_to_login_page()  # выполняем метод страницы — переходим на страницу логин
-    time.sleep(5)
-    page.should_be_login_link()
-    time.sleep(5)
-    # проверяем корректность перехода на с
+    page.should_be_login_link()  # проверяем ссылку на страницу логина
+    # проверяем корректность перехода на страницу логина
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
