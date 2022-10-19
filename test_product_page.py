63aa1dd0fecd8a0352ec5e312dd632fe8acd2d35
xxxREDBORNxxx <102207872+xxxREDBORNxxx@users.noreply.github.com>
2022-10-19 23:47:21 +0400

    update test_product_page

--- test_product_page.py
+++ test_product_page.py
@@ -1,10 +1,18 @@
+import pytest
+
 from .pages.product_page import ProductPage
 
 
-def test_guest_can_add_product_to_basket(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+@pytest.mark.parametrize('param',
+                         [*range(1, 7), pytest.param(7, marks=pytest.mark.xfail(reason='bugged')), *range(8, 10)])
+def test_guest_can_add_product_to_basket(browser, param):
+    link = f"http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_" \
+           f"207/?promo=offer{param}"
     page = ProductPage(browser, link)  # инициализируем Page Object
     page.open()  # открываем страницу
     page.check_click_basket()  # проверяем кликабельность кнопки "Добавить в корзину"
     page.add_to_basket()  # добавляем товар в корзину
     page.solve_quiz_and_get_code()  # рассчитываем формулу из алерта и получаем ответ для курса
+    page.compare_product_names()  # сравниваем название товара в корзине и в сообщении статуса
+
+
