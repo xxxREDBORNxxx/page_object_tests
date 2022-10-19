ecab921214632b3dcac1ce3c47871b3fe2222cb5
xxxREDBORNxxx <102207872+xxxREDBORNxxx@users.noreply.github.com>
2022-10-19 23:40:11 +0400

    alert code commented out

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,11 +1,12 @@
 import math
+import time
 
 from selenium.common.exceptions import NoAlertPresentException
 from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
-    def __init__(self, browser, url, timeout=10):
+    def __init__(self, browser, url, timeout=5):
         self.browser = browser
         self.url = url
         self.browser.implicitly_wait(timeout)
@@ -24,12 +25,14 @@ class BasePage:
         alert = self.browser.switch_to.alert
         x = alert.text.split(" ")[2]
         answer = str(math.log(abs((12 * math.sin(float(x))))))
+        time.sleep(2)
         alert.send_keys(answer)
         alert.accept()
-        try:
-            alert = self.browser.switch_to.alert
-            alert_text = alert.text
-            print(f"Your code: {alert_text}")
-            alert.accept()
-        except NoAlertPresentException:
-            print("No second alert presented")
+        time.sleep(20)
+        # try:
+        #     alert = self.browser.switch_to.alert
+        #     alert_text = alert.text
+        #     print(f"Your code: {alert_text}")
+        #     alert.accept()
+        # except NoAlertPresentException:
+        #     print("No second alert presented")
