3b2daa826af3f3cbcbe2e8aabb154ad416e5ad0d
xxxREDBORNxxx <102207872+xxxREDBORNxxx@users.noreply.github.com>
2022-10-19 23:41:44 +0400

    add product_page selectors

--- pages/locators.py
+++ pages/locators.py
@@ -12,4 +12,5 @@ class LoginPageLocators:
 
 class ProductPageLocators:
     BASKET_LINK = (By.CSS_SELECTOR, ".add-to-basket .btn-add-to-basket")
-
+    NAME_PRODUCT_LINK = (By.CSS_SELECTOR, ".product_page .product_main h1")
+    STATUS_NAME_PRODUCT_LINK = (By.CSS_SELECTOR, ".alert:first-child strong")
