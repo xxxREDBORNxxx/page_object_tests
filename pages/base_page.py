f0ac3fdf9b38ff8cfd3e7fed93f219c2dd1d8f46
xxxREDBORNxxx <102207872+xxxREDBORNxxx@users.noreply.github.com>
2022-10-20 19:19:18 +0400

    add new metods: is_notelement_present, is_disappeared

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,8 +1,10 @@
 import math
 import time
 
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException, TimeoutException
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import NoAlertPresentException
-from selenium.common.exceptions import NoSuchElementException
 
 
 class BasePage:
@@ -36,3 +38,22 @@ class BasePage:
         #     alert.accept()
         # except NoAlertPresentException:
         #     print("No second alert presented")
+
+    def is_not_element_present(self, how, what, timeout=4):
+        """Упадет, как только увидит искомый элемент. Не появился: успех/true"""
+        try:
+            WebDriverWait(self.browser, timeout).until(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return True
+
+        return False
+
+    def is_disappeared(self, how, what, timeout=4):
+        """Ждем пока не исчезнет. Исчез: успех/true"""
+        try:
+            WebDriverWait(self.browser, timeout, 1, TimeoutException).\
+                until_not(EC.presence_of_element_located((how, what)))
+        except TimeoutException:
+            return False
+
+        return True
